3cd77e09508c8681e1a7bebf0b33b387075c7ca0
PameAGP <pamelaagp2014@gmail.com>
2024-01-25 20:08:32 -0300

    Productos vizualizables

--- public/js/index.js
+++ public/js/index.js
@@ -4,3 +4,19 @@ const socket = io()
 socket.on('mensaje1', (data) => {
 console.log(data)
 })
+
+const addProductVisual = () => {
+    const prod = {
+      title: document.getElementById('title-product').value,
+      description: document.getElementById('description-product').value,
+      price: document.getElementById('price-product').value,
+      thumbnail: document.getElementById('thum-product').value,
+      code: document.getElementById('code-product').value,
+      stock: document.getElementById('stock-product').value,
+      category: document.getElementById('category-product'),value
+    }
+    
+    console.log(prod)
+
+    return false
+  }
--- router/home.route.js
+++ router/home.route.js
@@ -1,13 +1,29 @@
 const express = require ('express')
 
+const fs = require('fs').promises;
+
 const {Router} = express
 
 const router = new Router()
 
-router.get('/home', (req, res) => {
-    
-    res.render("home")
+
+router.get('/home', async (req, res) => {
+  
+  const data = await fs.readFile('productos.json', 'utf8');
+  const productos = JSON.parse(data);
+
+    res.render('home', {productos:productos})
   })
+
+  router.get('/realtimeproducts', async (req, res) => {
+  
+    const data = await fs.readFile('productos.json', 'utf8');
+    const productos = JSON.parse(data);
+  
+      res.render("realtimeproducts", {productos:productos})
+    })
+  
+
 
 
   module.exports = router
